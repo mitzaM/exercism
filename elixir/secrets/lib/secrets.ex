4d11c128fdf83f4a14a7aaa4d5867026ed41b560
Mihai Man <mihai.man@rover.com>
2023-02-05 17:54:05 +0300

    Elixir: Secrets

---
+++ elixir/secrets/lib/secrets.ex
@@ -0,0 +1,19 @@
+defmodule Secrets do
+  def secret_add(secret), do: fn param -> param + secret end
+
+  def secret_subtract(secret), do: &(&1 - secret)
+
+  def secret_multiply(secret), do: &(&1 * secret)
+
+  def secret_divide(secret) when secret != 0, do: &(div(&1, secret))
+
+  def secret_and(secret), do: &(Bitwise.band(&1, secret))
+
+  def secret_xor(secret), do: &(Bitwise.bxor(&1, secret))
+
+  def secret_combine(secret_function1, secret_function2), do: fn param ->
+      param
+      |> secret_function1.()
+      |> secret_function2.()
+  end
+end
