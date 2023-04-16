00d10969dc6d8f58a44759b98418edbfc61bd803
Mihai Man <mihai.man@rover.com>
2023-04-17 01:15:56 +0300

    R: Raindrops

---
+++ r/raindrops/raindrops.R
@@ -0,0 +1,12 @@
+raindrops <- function(number) {
+  result <- ""
+  if (number %% 3 == 0)
+    result <- paste0(result, "Pling")
+  if (number %% 5 == 0)
+    result <- paste0(result, "Plang")
+  if (number %% 7 == 0)
+    result <- paste0(result, "Plong")
+  if (result == "")
+    result <- as.character(number)
+  result
+}
