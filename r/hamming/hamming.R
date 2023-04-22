62d0989ce8f14da9c963388e08a5da6cd687e0cb
Mihai Man <mihai.man@rover.com>
2023-04-22 03:01:28 +0300

    R: Hamming - v2

--- r/hamming/hamming.R
+++ r/hamming/hamming.R
@@ -3,9 +3,6 @@
 hamming <- function(strand1, strand2) {
   if (nchar(strand1) != nchar(strand2))
     stop("Strands have different length")
-  if (strand1 == strand2)
-    return(0)
-  s.a <- strsplit(strand1, "")[[1]]
-  s.b <- strsplit(strand2, "")[[1]]
-  nchar(paste(s.a[s.a != s.b], collapse=""))
+  s <- strsplit(c(strand1, strand2), "")
+  sum(s[[1]] != s[[2]])
 }
